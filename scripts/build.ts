94b10d23eec148a2b35b4a59397c8987e000faae
takker99 <37929109+takker99@users.noreply.github.com>
2023-08-16 18:03:01 +0900

    :bug: Forgot to replace `ESBUILD_VERSION`

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,25 +1,32 @@
 // cf. https://github.com/evanw/esbuild/blob/d751dfb82002d332aa4dbfa89c74d25203d28123/scripts/esbuild.js#L85-L107
 
-import { build, stop } from "https://deno.land/x/esbuild@v0.17.19/mod.js";
+import {
+  build,
+  BuildOptions,
+  stop,
+} from "https://deno.land/x/esbuild@v0.17.19/mod.js";
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.1/mod.ts";
+import { ESBUILD_VERSION } from "../version.ts";
 
 const worker = await build({
-  plugins: [...denoPlugins()],
   entryPoints: [new URL("../worker.ts", import.meta.url).href],
+  plugins: [...denoPlugins()],
   write: false,
   bundle: true,
   minify: true,
-  legalComments: "eof",
   format: "esm",
+  legalComments: "eof",
+  define: { ESBUILD_VERSION: `"${ESBUILD_VERSION}"` },
 });
 const client = await build({
-  plugins: [...denoPlugins()],
   entryPoints: [new URL("../mod.ts", import.meta.url).href],
+  plugins: [...denoPlugins()],
   write: false,
   bundle: true,
   minify: true,
-  legalComments: "eof",
   format: "esm",
+  legalComments: "eof",
+  define: { ESBUILD_VERSION: `"${ESBUILD_VERSION}"` },
 });
 
 await Deno.writeTextFile("./worker.js", worker.outputFiles[0].text);
